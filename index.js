5eb2d35232f41582d89782407092a821a6359a45
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2018-01-22 17:17:29 +0100

    Initial implementation

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const map = require('broccoli-stew').map;
+const path = require('path');
+
 module.exports = {
-  name: 'ember-cli-lazysizes'
+  name: 'ember-cli-lazysizes',
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    this.addonOptions = app.options[this.name] || {};
+    this.import('vendor/lazysizes.js');
+  },
+
+  treeForVendor() {
+    let lazySizesPath = path.dirname(require.resolve('lazysizes'));
+
+    return map(this.treeGenerator(lazySizesPath), 'lazysizes.js', (content) => `if (typeof FastBoot === 'undefined') {
+      window.lazySizesConfig = ${JSON.stringify(this.addonOptions)};
+      ${content}
+    }`);
+  }
 };
---
+++ tests/acceptance/lazysizes-test.js
@@ -0,0 +1,13 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | lazysizes');
+
+test('it creates the `lazySizes` global and overwrittes the `lazyClass`', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.ok(window.lazySizes);
+    assert.equal(window.lazySizesConfig.lazyClass, 'lazy');
+  });
+});
