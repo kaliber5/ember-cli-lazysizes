8b0edbc0b9032dfe1c041adca618cefcb3f19e55
andreasschacht <schacht@kaliber5.de>
2018-02-22 15:35:47 +0100

    refactor treeForVendor

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const map = require('broccoli-stew').map;
 const path = require('path');
 const find = require('broccoli-stew').find;
 const transform = require('fastboot-transform');
+const mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-cli-lazysizes',
@@ -16,33 +17,28 @@ module.exports = {
     delete this.addonOptions.plugins;
 
     this.plugins.forEach((plugin) => {
-      this.import(`vendor/plugins/${plugin}/ls.${plugin}.js`, { using: [{ transformation: 'fastboot-transform' }] });
+      this.import(`vendor/plugins/${plugin}/ls.${plugin}.js`);
     });
 
-    this.import(`vendor/lazysizes.js`, { using: [{ transformation: 'inject-lazysizes-config' }] });
+    this.import(`vendor/lazysizes.js`);
   },
 
-  importTransforms() {
-    return {
-      'inject-lazysizes-config': (tree) => {
-        return transform(map(tree, (content) => {
-          return `window.lazySizesConfig = ${JSON.stringify(this.addonOptions)};
-          ${content}`;
-        }));
-      },
-      'fastboot-transform': transform
+  treeForVendor(tree) {
+    let trees = [tree];
+
+    let lazyNodeTree = this.treeGenerator(path.dirname(require.resolve('lazysizes')));
+    if (this.plugins.length > 0) {
+      let include = [];
+      this.plugins.forEach((plugin) => {
+        include.push(`plugins/${plugin}/ls.${plugin}.js`);
+      });
+
+      trees.push(transform(find(lazyNodeTree, { include })));
     }
-  },
-
-  treeForVendor() {
-    let include = [];
-
-    let lazySizesPath = path.dirname(require.resolve('lazysizes'));
-    include.push('lazysizes.js');
-    this.plugins.forEach((plugin) => {
-      include.push(`plugins/${plugin}/ls.${plugin}.js`);
-    });
-
-    return find(this.treeGenerator(lazySizesPath), { include });
+    trees.push(transform(map(find(lazyNodeTree, 'lazysizes.js'), (content) => {
+      return `window.lazySizesConfig = ${JSON.stringify(this.addonOptions)};
+          ${content}`;
+    })));
+    return mergeTrees(trees, { overwrite: true });
   }
 };
